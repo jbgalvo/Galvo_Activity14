b227e29af9dab290468555d25f39df0571a13175
Jb Galvo <jbmgalvo16@gmail.com>
2023-10-20 07:39:20 +0800

    added pages home, about, services, contact and layout jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
+import Services from "./pages/Services.jsx";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 function App() {
  
   return (
     <>
-      <h1>App Component</h1>
+      <Services />
     </>
   )
 }
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,9 @@
+function Layout() {
+  return (
+    <>
+      <h1>Layout Component</h1>
+    </>
+  )
+}
+
+export default Layout;
